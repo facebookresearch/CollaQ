24a05dca37b83dc6ae6290d402a08dca0267264f
tianjunz8821 <66745036+tianjunz8821@users.noreply.github.com>
2020-10-06 10:52:34 -0700

    Update basic_controller.py

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from modules.agents import REGISTRY as agent_REGISTRY
 from components.action_selectors import REGISTRY as action_REGISTRY
 import torch as th

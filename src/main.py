29206d2f1c72885e6cf36d53176ed486e38ce0a9
tianjunz8821 <66745036+tianjunz8821@users.noreply.github.com>
2020-10-06 10:48:50 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-Copyright (c) Facebook, Inc. and its affiliates.
-All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
 
-This source code is licensed under the license found in the
-LICENSE file in the root directory of this source tree.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 import numpy as np
 import os
